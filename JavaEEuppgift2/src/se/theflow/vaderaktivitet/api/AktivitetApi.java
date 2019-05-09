ebd491576bef6fe58701686ea65e00c2e7954232
Lars Strömberg <stromberg.lars@gmail.com>
2019-05-09 14:04:18 +0200

    bundit klasser till databasen

--- JavaEEuppgift2/src/se/theflow/vaderaktivitet/api/AktivitetApi.java
+++ JavaEEuppgift2/src/se/theflow/vaderaktivitet/api/AktivitetApi.java
@@ -1,4 +1,19 @@
 package se.theflow.vaderaktivitet.api;
 
-public class AktivitetApi {
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Application;
+import javax.ws.rs.core.MediaType;
+import java.util.List;
+
+@ApplicationPath("/aktivitetapi")
+@Path("/v1.0")
+public class AktivitetApi extends Application {
+
+    @GET
+    @Path("/aktivitet")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Activity>
 }
