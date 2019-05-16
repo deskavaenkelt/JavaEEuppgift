7b8628fefe24963024da8600b1f037342c389995
Sofia Ridderstad <sofia.ridderstad@gmail.com>
2019-05-16 15:42:21 +0200

    Corection code

--- JavaEEuppgift2/src/se/theflow/vaderaktivitet/repository/PlatsRepository.java
+++ JavaEEuppgift2/src/se/theflow/vaderaktivitet/repository/PlatsRepository.java
@@ -1,51 +1,5 @@
 package se.theflow.vaderaktivitet.repository;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-
 public class PlatsRepository {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Integer platsId;
-
-    private String platsNamn;
-
-    private float platsLongitude;
-
-    private float platsLatitude;
-
-
-    public Integer getPlatsId() {
-        return platsId;
-    }
-
-    public void setPlatsId(Integer platsId) {
-        this.platsId = platsId;
-    }
-
-    public String getPlatsNamn() {
-        return platsNamn;
-    }
-
-    public void setPlatsNamn(String platsNamn) {
-        this.platsNamn = platsNamn;
-    }
-
-    public float getPlatsLongitude() {
-        return platsLongitude;
-    }
-
-    public void setPlatsLongitude(float platsLongitude) {
-        this.platsLongitude = platsLongitude;
-    }
-
-    public float getPlatsLatitude() {
-        return platsLatitude;
-    }
-
-    public void setPlatsLatitude(float platsLatitude) {
-        this.platsLatitude = platsLatitude;
-    }
 }
