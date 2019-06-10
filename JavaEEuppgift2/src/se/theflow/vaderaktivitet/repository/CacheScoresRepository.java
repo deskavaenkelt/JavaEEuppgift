de0973bd5cd2126bc5a30b08e53abb78b99fa6cb
Lars Strömberg <stromberg.lars@gmail.com>
2019-06-10 14:47:40 +0200

    something

--- JavaEEuppgift2/src/se/theflow/vaderaktivitet/repository/CacheScoresRepository.java
+++ JavaEEuppgift2/src/se/theflow/vaderaktivitet/repository/CacheScoresRepository.java
@@ -30,4 +30,43 @@ public class CacheScoresRepository {
         entityManager.getTransaction().commit();
 
     }
+
+    public void updateFishing(int id, float fishing) {
+        entityManager.getTransaction().begin();
+        CacheScoresModel updateScore = em.find(CacheScoresModel.class, id);
+        updateScore.setFishing(fishing);
+        entityManager.getTransaction().commit();
+
+    }
+
+    public void updateOutdoorSeating(int id, float outdoorSeating) {
+        entityManager.getTransaction().begin();
+        CacheScoresModel updateScore = em.find(CacheScoresModel.class, id);
+        updateScore.setOutdoorSeating(outdoorSeating);
+        entityManager.getTransaction().commit();
+
+    }
+
+    public void updateSunBathing(int id, float sunBathing) {
+        entityManager.getTransaction().begin();
+        CacheScoresModel updateScore = em.find(CacheScoresModel.class, id);
+        updateScore.setSunBathing(sunBathing);
+        entityManager.getTransaction().commit();
+
+    }
+
+    public void updateVolleyBall(int id, float volleyBall) {
+        entityManager.getTransaction().begin();
+        CacheScoresModel updateScore = em.find(CacheScoresModel.class, id);
+        updateScore.setVolleyBall(volleyBall);
+        entityManager.getTransaction().commit();
+    }
+
+    public void updateWindSurfing(int id, float windSurfing) {
+        entityManager.getTransaction().begin();
+        CacheScoresModel updateScore = em.find(CacheScoresModel.class, id);
+        updateScore.setWindSurfing(windSurfing);
+        entityManager.getTransaction().commit();
+
+    }
 }
