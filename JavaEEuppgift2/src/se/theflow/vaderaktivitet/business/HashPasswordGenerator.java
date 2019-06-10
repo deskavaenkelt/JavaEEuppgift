abb3d1721f209039beec36a13126474e336705f1
Lars Strömberg <stromberg.lars@gmail.com>
2019-06-10 14:47:08 +0200

    Create new user and update scores in db

--- JavaEEuppgift2/src/se/theflow/vaderaktivitet/business/HashPasswordGenerator.java
+++ JavaEEuppgift2/src/se/theflow/vaderaktivitet/business/HashPasswordGenerator.java
@@ -1,6 +1,6 @@
 package se.theflow.vaderaktivitet.business;
 
-import se.theflow.vaderaktivitet.repository.UserLogin;
+import se.theflow.vaderaktivitet.repository.UserRepository;
 
 import javax.inject.Inject;
 import java.security.MessageDigest;
@@ -10,7 +10,14 @@ import java.util.Random;
 public class HashPasswordGenerator {        // work in progress
 
     @Inject
-    private UserLogin userLogin;
+    private UserRepository userRepository;
+
+    public String generateASalt(String passwordToMakeHashFrom) {
+        return generateRandomSaltString(10000);
+    }
+    public String generateHachedPassword(String salt, String passwordToMakeHashFrom) {
+        return generateHashString(salt, passwordToMakeHashFrom);
+    }
 
     public String makeHashHappen(String passwordToMakeHashFrom) {
         String salt = generateRandomSaltString(10000);
@@ -23,7 +30,7 @@ public class HashPasswordGenerator {        // work in progress
     }
 
     public String checkPassword(String passwordToMakeHashFrom, int id) {
-        String salt = userLogin.findUserByUserName(id).getUserSalt();
+        String salt = userRepository.findUserByUserName(id).getUserSalt();
         String hash = generateHashString(salt, passwordToMakeHashFrom);
 
         return hash;
