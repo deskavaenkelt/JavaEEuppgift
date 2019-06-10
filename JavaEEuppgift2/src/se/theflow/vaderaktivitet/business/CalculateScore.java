692864bbc79de03525cdb38a054417213c7676f9
Sofia Ridderstad <sofia.ridderstad@gmail.com>
2019-06-10 13:47:34 +0200

    Remade calculate score

--- JavaEEuppgift2/src/se/theflow/vaderaktivitet/business/CalculateScore.java
+++ JavaEEuppgift2/src/se/theflow/vaderaktivitet/business/CalculateScore.java
@@ -6,66 +6,26 @@ public class CalculateScore {
 
     public float HighScoreTempVindRain(float Optimal, float Now) {
         float Difference;
-        float Point = 0;
+        float Point = 100;
+
         Difference = Optimal-Now;
-        if(Difference == 0) {
-            Point = 15;
-        }else if(Difference == 1) {
-            Point = 14;
-        }else if(Difference == 2) {
-            Point = 13;
-        }else if(Difference == 3) {
-            Point = 12;
-        }else if(Difference == 4) {
-            Point = 11;
-        }else if(Difference == 5) {
-            Point = 10;
-        }else if(Difference == 6) {
-            Point = 9;
-        }else if(Difference == 7) {
-            Point = 8;
-        }else if(Difference == 8) {
-            Point = 7;
-        }else if(Difference == 9) {
-            Point = 6;
-        }else if(Difference == 10) {
-            Point = 5;
-        }else if(Difference == 11) {
-            Point = 4;
-        }else if(Difference == 12) {
-            Point = 3;
-        }else if(Difference == 13) {
-            Point = 2;
-        }else if(Difference == 14) {
-            Point = 1;
-        }else if(Difference < 15) {
-            Point = 0;
+        Difference = Difference * 10;
+
+        for(int i = 0; i < Difference; i++) {
+            Point --;
         }
         return Point;
     }
 
     public float HighScoreCloud(float Optimal, float Now) {
         float Difference;
-        float Point = 0;
+        float Point = 100;
+        
         Difference = Optimal-Now;
-        if(Difference == 0) {
-            Point = 15;
-        }else if(Difference == 1) {
-            Point = 13;
-        }else if(Difference == 2) {
-            Point = 11;
-        }else if(Difference == 3) {
-            Point = 9;
-        }else if(Difference == 4) {
-            Point = 7;
-        }else if(Difference == 5) {
-            Point = 5;
-        }else if(Difference == 6) {
-            Point = 3;
-        }else if(Difference == 7) {
-            Point = 1;
+
+        for(int i = 0; i < Difference; i++) {
+            Point = Point - 10;
         }
         return Point;
     }
-
 }
