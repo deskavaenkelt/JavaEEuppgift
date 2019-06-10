1d5228a11c07b51227f0cce1e03c2ad6fac5f2f7
Sofia Ridderstad <sofia.ridderstad@gmail.com>
2019-06-10 13:52:59 +0200

    Makeup

--- JavaEEuppgift2/src/se/theflow/vaderaktivitet/business/CompromiseMagic.java
+++ JavaEEuppgift2/src/se/theflow/vaderaktivitet/business/CompromiseMagic.java
@@ -12,18 +12,18 @@ public class CompromiseMagic {
     public void updateAllPoints(){
         //0 = fishing, 1 = outdoorsEating, 2 = sunBathing, 3 = volleyboll, 4 = vindsurfing
         for(int id = 0; id < 4; id++){
-        float tempFloatArray[] = new float[4];
-        float tempOptimalArray[] = new float[4];
-        tempOptimalArray = weatheroptimalbl.getActivityById(id);
-        float celOptimal = tempOptimalArray[0];
-        float windOptimal = tempOptimalArray[1];
-        float cloudOptimal = tempOptimalArray[2];
-        float rainOptimal = tempOptimalArray[3];
-        tempFloatArray = smhi.weatherFetcher(smhi.timeAdder(0), smhi.urlGenerator(id));
-        float celPoints = calculatescore.HighScoreTempVindRain(celOptimal, tempFloatArray[0]);
-        float cloudPoints = calculatescore.HighScoreCloud(cloudOptimal, tempFloatArray[1]);
-        float windPoints = calculatescore.HighScoreTempVindRain(windOptimal, tempFloatArray[2]);
-        float rainPoints = calculatescore.HighScoreTempVindRain(rainOptimal, tempFloatArray[3]);
-    }
+            float tempFloatArray[] = new float[4];
+            float tempOptimalArray[] = new float[4];
+            tempOptimalArray = weatheroptimalbl.getActivityById(id);
+            float celOptimal = tempOptimalArray[0];
+            float windOptimal = tempOptimalArray[1];
+            float cloudOptimal = tempOptimalArray[2];
+            float rainOptimal = tempOptimalArray[3];
+            tempFloatArray = smhi.weatherFetcher(smhi.timeAdder(0), smhi.urlGenerator(id));
+            float celPoints = calculatescore.HighScoreTempVindRain(celOptimal, tempFloatArray[0]);
+            float cloudPoints = calculatescore.HighScoreCloud(cloudOptimal, tempFloatArray[1]);
+            float windPoints = calculatescore.HighScoreTempVindRain(windOptimal, tempFloatArray[2]);
+            float rainPoints = calculatescore.HighScoreTempVindRain(rainOptimal, tempFloatArray[3]);
+        }
     }
 }
